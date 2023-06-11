7c2ca1f5ad7aca087855f4cb4fd4319f81905caa
Abhishek Maurya <mauryaabhishek1508@gmail.com>
2023-06-11 13:28:13 +0530

    All the files are added at once

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+// function test(){
+//     var a = "3";
+//     var b = "8";
+
+//     var temp = a ;
+//     a = b;
+//      b = temp;
+
+//      console.log("a is " + a);
+//      console.log("b is " + b);
+// }
+// test();
+
+
+// making the word to upper case
+ var name = "abhishek";
+
+console.log(name.toCamelCase());
